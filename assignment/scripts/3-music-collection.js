86786f16d3f3d3c53c8134dc8fffffdfb4739069
Halima Moalim <halimaamoalim@gmail.com>
2022-07-05 11:57:48 -0500

    show collection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,62 @@
 console.log('***** Music Collection *****')
+let collection = [{
+    title: 'Lungs',
+    artist: 'Florence and The Machine',
+    yearPublished: 2009
+},
+{
+    title: 'Dance Fever',
+    artist: 'Florence and The Machine',
+    yearPublished: 2022,
+},
+{
+    title:'Planet Her',  
+    artist: 'Doja Cat',
+    yearPublished: 2021,
+},
+{
+    title: 'Sour',
+    artist: 'Olivia Rodrigo',
+    yearPublished: 2021,
+},
+{
+    title: 'Golden Hour',
+    artist: 'Kacey Musgraves',
+    yearPublished: 2018,
+},
+{
+    title: 'AM',
+    artist: 'Arctic Monkeys',
+    yearPublished: 2013,
+}];
+
+console.log(collection.title, 'Lungs');
+
+let myMusic = {
+    title: ' ',
+    artist: ' ',
+    yearPublished: ' ',
+};
+
+function addToCollection(titleInput, artistInput, yearPublishedInput){
+    collection.push(myMusic);
+    return collection;
+}
+console.table(addToCollection());
+
+
+function showCollection(collection){
+    for(let i = 0; i < collection.length; i++){
+        console.log(collection);
+     }
+
+}//end of showCollection function
+console.log(showCollection());
+
+
+function findByArtist(){
+
+
+
+
+}
